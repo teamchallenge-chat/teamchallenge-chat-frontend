c0755e0487468e0958e5a6c7cd6da2d8147f07ed
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-03-22 23:56:41 +0100

    feat: implement base modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { FormControlComponent } from './components/form-control/form-control.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
-  declarations: [ButtonComponent, FormControlComponent],
-  exports: [ButtonComponent, FormControlComponent],
+  declarations: [ButtonComponent, FormControlComponent, ModalComponent],
+  exports: [ButtonComponent, FormControlComponent, ModalComponent],
   imports: [CommonModule, ReactiveFormsModule],
 })
 export class SharedModule {}
