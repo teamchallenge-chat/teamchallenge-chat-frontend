65779e2aff3d33473e02c52caa2ca573ef9ea858
Viktor-1995 <madebyvityok@ukr.net>
2024-03-26 18:16:22 +0200

    validators new password

--- src/app/auth/pages/new-password/new-password.component.ts
+++ src/app/auth/pages/new-password/new-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AppRoutes } from '@core/enums/routes.enum';
+import { matchValidator } from '@shared/validators/match.validator';
 
 @Component({
   selector: 'app-new-password',
@@ -12,17 +13,22 @@ export class NewPasswordComponent {
 
   protected readonly NEW_PASSWORD_PAGE_ROUTE = `/${AppRoutes.NEW_PASSWORD_PAGE_ROUTE}`;
 
-  newPasswordForm = this.fb.group({
-    password: [
-      '',
-      [
-        Validators.required,
-        Validators.minLength(8),
-        Validators.pattern(
-          /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/
-        ),
+  newPasswordForm = this.fb.group(
+    {
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(
+            /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/
+          ),
+        ],
       ],
-    ],
-    confirmPassword: ['', [Validators.required]],
-  });
+      confirmPassword: ['', [Validators.required]],
+    },
+    {
+      validators: matchValidator('password', 'confirmPassword'),
+    }
+  );
 }
